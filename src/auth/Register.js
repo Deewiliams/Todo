67d6ebd89f19e7437d00b6318f8e14dee651b51e
Desire <desireirankundawilliams@gmail.com>
2023-10-10 22:40:34 +0200

    created a register page

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -3,7 +3,9 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import { Button, TextField } from "@mui/material";
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { Auth } from "aws-amplify";
+import register from "../images/register.svg";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -14,37 +16,69 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Register() {
+  const [username, setUserName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  async function signUp() {
+    try {
+      const { user } = await Auth.signUp({
+        username,
+        password,
+      });
+      console.log(user);
+    } catch (error) {
+      console.log("error signing up:", error);
+    }
+  }
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Item>xs=8</Item>
+    <>
+      <Box sx={{ flexGrow: 1, marginTop: "150px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <img src={register} alt="register " />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Typography style={{fontSize: "50px", textAlign: "center"}}>
+                Welcome to Todo 
+            </Typography>
+            <Typography style={{fontSize: "50px", textAlign: "center"}}>
+            Application
+            </Typography>
+            <Box sx={{ flexGrow: 1, marginTop: "50px" }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                    placeholder="todo@gamil.com"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <Button onClick={signUp} fullWidth variant="contained" style={{textTransform: 'none'}}>
+                    Sign up
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            placeholder="todo@gamil.com"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            type="password"
-            placeholder="Password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button fullWidth variant="contained">
-            Register
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 }
