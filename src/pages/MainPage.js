4b752ae0ad50914ad63901336acaa9ef7dc7b145
Desire <desireirankundawilliams@gmail.com>
2023-10-11 15:26:51 +0200

    cleaned the code

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,17 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import AddTodo from "../component/AddTodo";
 
 export default function MainPage() {
   const [auth, setAuth] = React.useState(true);
@@ -30,7 +31,6 @@ export default function MainPage() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-    
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -61,13 +61,13 @@ export default function MainPage() {
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
@@ -79,6 +79,8 @@ export default function MainPage() {
           )}
         </Toolbar>
       </AppBar>
+      <br />
+      <AddTodo />
     </Box>
   );
 }
